0cb20ba0bfd74ca469a7d2f1bed764ca3f0429d6
AlinaGay <gayfullina.a@gmail.com>
2026-03-07 19:25:44 +0100

    Add resume uploaded to frontend

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,24 @@
 import os
 import shutil
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
-@app.post("/upload/")
-async def upload_document(file: UploadFile):
+@app.post("/upload_resume/")
+async def upload_resume(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [resumeUploaded, setResumeUploaded] = useState(false)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const handleResumeUpload = async (e) => {
+        const file = e.target.files[0];
+        if (!file) return;
+
+        const formData = new FormData();
+        formData.append("file", file);
+
+        const res = await fetch("http://localhost:8000/upload_resume/", {
+            method: "POST",
+            body: formData,
+        });
+        if (res.ok) setResumeUploaded(true);
+    };
+
+    return (
+        <div>
+            <h1>Job apllication assistant</h1>
+            <input type="file" accept="application/pdf" onChange={handleResumeUpload} />
+            {resumeUploaded && <p>Resume has uploaded</p>}
+        </div>
+    );
 }
 
-export default App
+export default App;
